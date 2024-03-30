38a804b88733b5bb4d93216e9586b3adee273483
abulo <abulo.hoo@gmail.com>
2024-03-30 21:02:26 +0800

    config🔧: 自动构建工具

--- cloud/build.sh
+++ cloud/build.sh
@@ -2,7 +2,8 @@
 path=$(cd `dirname $0`; pwd)
 # go-bindata -o=initial/view/view.go -pkg=view  ./view/...
 gohash=$(git rev-parse HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD)
 date=$(date +"%Y-%m-%d %H:%M:%S")
-flags="-X 'main.BuildTime=$date' -X 'main.BuildVersion=$gohash'"
+flags="-X 'main.BuildTime=$date' -X 'main.BuildVersion=$branch/$gohash'"
 GOOS=linux GOARCH=amd64 go build --ldflags="$flags" -o $path/bin/hertz $path/cmd/hertz/hertz.go
 GOOS=linux GOARCH=amd64 go build --ldflags="$flags" -o $path/bin/grpc $path/cmd/grpc/grpc.go
